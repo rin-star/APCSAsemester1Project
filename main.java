9fa3c2bb19d2119616a70a4eb7994def72064eeb
rin-star <148642180+rin-star@users.noreply.github.com>
2025-12-12 10:40:58 -0800

    Co-authored-by: ashlee1231 <ashlee1231@users.noreply.github.com> Co-authored-by: GnatGo <GnatGo@users.noreply.github.com>

--- main.java
+++ main.java
@@ -46,21 +46,51 @@ public class main {
     
     public static void selection(int userNum, Pokemon pokemon) {
         if (userNum == 1) {
-            pokemon.feed();
+            pokemon.returnStats();
         }
         else if (userNum == 2) {
-            pokemon.play();
+            pokemon.feed();
         }
         else if (userNum == 3) {
-            pokemon.returnStats();
+            pokemon.play();
+        }
+        else if (userNum == 4) {
+            pokemon.giveBath();
+        }
+        else if (userNum == 5) {
+            pokemon.heal();
+        }
+        else if (userNum == 6) {
+            pokemon.evolve();
+        }
+        else if (userNum == 7) {
+            pokemon.fightTrainer();
+        }
+        else if (userNum == 8) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("What candy do you want to feed your pokemon?(big, small): ");
+            String candyInput = input.nextLine();
+            pokemon.feedCandy(candyInput);
+        }
+        else if (userNum == 9) {
+            pokemon.rest();
+        }
+        else {
+            System.out.println("That's not a valid input.");
         }
     }
     //menu
     public static void menu() {
         System.out.println("Please make a selection from the options below.");
-        System.out.println("1. Feed");
-        System.out.println("2. Play");
-        System.out.println("3. Show stats");
+        System.out.println("1. Show stats");
+        System.out.println("2. Feed");
+        System.out.println("3. Play");
+        System.out.println("4. Give Bath");
+        System.out.println("5. Heal");
+        System.out.println("6. Evolve");
+        System.out.println("7. Fight Trainer");
+        System.out.println("8. Feed Candy");
+        System.out.println("9. Rest");
     }
 
     
